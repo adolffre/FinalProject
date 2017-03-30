00ac84e5d8ef8a57532f3c91f7c7ba56e156238c
Adolf Jurgens <adolffre@gmail.com>
2017-03-30 18:22:21 +0200

    initial commit

---
+++ FinalProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  FinalProject
+//
+//  Created by Adolf Jurgens Freitas on 3/30/17.
+//  Copyright © 2017 Adolf Jurgens Freitas. All rights reserved.
+//
+
+import UIKit
+import Framework3
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let a = Test3()
+        print(a.test())
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
